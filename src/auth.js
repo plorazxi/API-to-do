ccc0bc26fe4fa59148ebc3e128668952d6c171ce
plorazxi <paulo.ss.loraschi@gmail.com>
2024-10-03 15:32:42 -0300

    Melhoria de tratamento de erros

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ const bc = require('bcrypt');
 const { randomInt } = require('node:crypto');
 const fs = require('fs');
 
-const users = JSON.parse(fs.readFileSync('DB/users.json', 'utf-8'));
+var users = JSON.parse(fs.readFileSync('DB/users.json', 'utf-8'));
 
 async function login(req, res) {
     const email = req.body.email;
@@ -11,8 +11,12 @@ async function login(req, res) {
     const result = users.filter((user) => {
         if(user.email==email) return user;
     });
-    if(result.length!=1) {
+    if(result.length>1) {
         res.status(506).send({erro: "banco de dados com falhas"});
+        return ;
+    } else if(result.length==0) {
+        res.status(401).send({erro: "email invalido"});
+        return ;
     }
     const user = result[0];
     if(await bc.compare(senha, user.senha)) {
@@ -29,7 +33,8 @@ async function login(req, res) {
             token: token
         });
     } else {
-        res.status(401).send({erro: "email ou senhas invalidas"});
+        res.status(401).send({erro: "senha invalida"});
+        return ;
     }
 }
 
