798ff8a2c6beefb2dc1235ea1d49430ee687edd9
plorazxi <paulo.ss.loraschi@gmail.com>
2024-10-06 00:15:40 -0300

    adicionando a função mudar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login, register } = require('./auth.js');
+const { login, register, mudar } = require('./auth.js');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +9,8 @@ app.use(express.json());
 app.get('/login', (req, res) => login(req, res));
 
 app.post('/register', (req, res) => register(req, res));
+
+app.put('/mudar-:tributo', (req, res) => mudar(req, res));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server on');
