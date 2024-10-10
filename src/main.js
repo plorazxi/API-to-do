7d78beb2dad34fa7256c198677804ffb97d40c56
plorazxi <paulo.ss.loraschi@gmail.com>
2024-10-09 22:09:29 -0300

    init de tasks.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const { login, register, mudar } = require('./auth.js');
+const { exibir, criar, deletar, alterar_task } = require('./tasks.js')
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
+// Rotas principais : serviço to-do
+app.get('/:token', (req, res) => exibir(req, res));
+
+// Rotas de autenticacao
 app.post('/login', (req, res) => login(req, res));
 
 app.post('/register', (req, res) => register(req, res));
 
 app.put('/mudar-:tributo', (req, res) => mudar(req, res));
 
+// Listen
 app.listen(process.env.PORT || 3000, () => {
     console.log('server on');
 });
